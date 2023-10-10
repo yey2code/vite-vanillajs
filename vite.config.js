322420ad5497471bdd5a233452bf8c2295f2d379
yey <yosarseeff@gmail.com>
2023-10-10 20:04:38 +0100

    init commit

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite'
 
-export default { 
-    base: `/vite-vanillajs/`,
-  };
+// export default { 
+//     base: `/vite-vanillajs/`,
+//   };
+
+  // https://vitejs.dev/config/
+export default defineConfig({
+  base: "./vite-vanillajs/",
+});
